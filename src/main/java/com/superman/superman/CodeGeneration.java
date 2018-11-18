bc2dfd0c4637e30ffa6d0908c6ceac11e71f608b
GGsnake <ljp462693266@outlook.com>
2018-11-18 08:02:07 +0800

    ss

---
+++ src/main/java/com/superman/superman/CodeGeneration.java
@@ -0,0 +1,74 @@
+package com.superman.superman;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.superman.superman.controller.MemberController;
+
+public class CodeGeneration {
+    /**
+     *
+     * @Title: main
+     * @Description: 生成
+     * @param args
+     */
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+//        E:\supman\hzhl-springboot-dev\src\main\java\com\superman\superman
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir("E://supman/hzhl-springboot-dev/src/main/java");
+        gc.setFileOverride(true);
+        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor("GGsnake");// 作者
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        mpg.setGlobalConfig(gc);
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("");
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/yetdwell");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix(new String[] { "" });// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[] { "emp" }); // 需要生成的表
+
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.superman.superman");
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("serviceImpl");
+        pc.setMapper("mapper");
+        pc.setEntity("model");
+        pc.setXml("xml");
+        mpg.setPackageInfo(pc);
+
+        // 执行生成
+        mpg.execute();
+
+    }
+}
---
+++ src/main/java/com/superman/superman/mapper/EmpMapper.java
@@ -0,0 +1,16 @@
+package com.superman.superman.mapper;
+
+import com.superman.superman.model.Emp;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author GGsnake
+ * @since 2018-11-10
+ */
+public interface EmpMapper extends BaseMapper<Emp> {
+
+}
