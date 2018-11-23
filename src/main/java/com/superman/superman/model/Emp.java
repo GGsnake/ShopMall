f148bfdaf2e4693cd8cd3e743338330eddd93668
GGsnake <ljp462693266@outlook.com>
2018-11-23 10:33:06 +0800

    ss

--- src/main/java/com/superman/superman/model/Emp.java
+++ src/main/java/com/superman/superman/model/Emp.java
@@ -1,81 +1,81 @@
-package com.superman.superman.model;
-
-import com.baomidou.mybatisplus.enums.IdType;
-import com.baomidou.mybatisplus.annotations.TableId;
-import com.baomidou.mybatisplus.annotations.TableField;
-import com.baomidou.mybatisplus.annotations.TableName;
-import java.io.Serializable;
-
-/**
- * <p>
- * 
- * </p>
- *
- * @author GGsnake
- * @since 2018-11-10
- */
-@TableName("emp")
-public class Emp implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @TableId(value = "id", type = IdType.AUTO)
-    private Integer id;
-    @TableField("last_name")
-    private String lastName;
-    private String email;
-    private String gender;
-    private Integer age;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Emp{" +
-        "id=" + id +
-        ", lastName=" + lastName +
-        ", email=" + email +
-        ", gender=" + gender +
-        ", age=" + age +
-        "}";
-    }
-}
+//package com.superman.superman.model;
+//
+//import com.baomidou.mybatisplus.enums.IdType;
+//import com.baomidou.mybatisplus.annotations.TableId;
+//import com.baomidou.mybatisplus.annotations.TableField;
+//import com.baomidou.mybatisplus.annotations.TableName;
+//import java.io.Serializable;
+//
+///**
+// * <p>
+// *
+// * </p>
+// *
+// * @author GGsnake
+// * @since 2018-11-10
+// */
+//@TableName("emp")
+//public class Emp implements Serializable {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    @TableId(value = "id", type = IdType.AUTO)
+//    private Integer id;
+//    @TableField("last_name")
+//    private String lastName;
+//    private String email;
+//    private String gender;
+//    private Integer age;
+//
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Emp{" +
+//        "id=" + id +
+//        ", lastName=" + lastName +
+//        ", email=" + email +
+//        ", gender=" + gender +
+//        ", age=" + age +
+//        "}";
+//    }
+//}
