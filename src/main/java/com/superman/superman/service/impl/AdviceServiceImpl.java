331295e35c808b76a477d6a0350d99e39275e1f0
GGsnake <ljp462693266@outlook.com>
2019-04-29 21:18:21 +0800

    压缩代码

--- src/main/java/com/superman/superman/service/impl/AdviceServiceImpl.java
+++ src/main/java/com/superman/superman/service/impl/AdviceServiceImpl.java
@@ -15,42 +15,43 @@ import java.util.Map;
 
 @Service("adviceService")
 public class AdviceServiceImpl implements AdviceService {
-	@Autowired
-	private SysAdviceDao adviceDao;
-	@Autowired
-	private AgentDao agentDao;
-	
-	@Override
-	public List<SysAdvice> queryList(Map<String, Object> map){
-		return adviceDao.queryList(map);
-	}
-	
-	@Override
-	public int queryTotal(Map<String, Object> map){
-		return adviceDao.queryTotal(map);
-	}
+    @Autowired
+    private SysAdviceDao adviceDao;
+    @Autowired
+    private AgentDao agentDao;
 
-	@Override
-	public List<SysJhAdviceOder> queryListOderAdvice(Long uid,PageParam pageParam) {
-		List<Long> var=new ArrayList();
-		var.add(uid);
-		List<Long> ids = agentDao.queryForAgentIdNew(uid.intValue());
-		if (ids!=null){
-			var.addAll(ids);
-		}
-		List<SysJhAdviceOder> sysJhAdviceOders = adviceDao.querySysOderAdvice(var,pageParam.getStartRow(),pageParam.getPageSize());
-		return sysJhAdviceOders;
-	}
-	@Override
-	public Integer countListOderAdvice(Long uid) {
-		List<Long> var=new ArrayList();
-		var.add(uid);
-		List<Long> ids = agentDao.queryForAgentIdNew(uid.intValue());
-		if (ids!=null){
-			var.addAll(ids);
-		}
-		Integer sum = adviceDao.countSysOderAdvice(var);
-		return sum;
-	}
+    @Override
+    public List<SysAdvice> queryList(Map<String, Object> map) {
+        return adviceDao.queryList(map);
+    }
+
+    @Override
+    public int queryTotal(Map<String, Object> map) {
+        return adviceDao.queryTotal(map);
+    }
+
+    @Override
+    public List<SysJhAdviceOder> queryListOderAdvice(Long uid, PageParam pageParam) {
+        List<Long> var = new ArrayList();
+        var.add(uid);
+        List<Long> ids = agentDao.queryForAgentIdNew(uid.intValue());
+        if (ids != null) {
+            var.addAll(ids);
+        }
+        List<SysJhAdviceOder> sysJhAdviceOders = adviceDao.querySysOderAdvice(var, pageParam.getStartRow(), pageParam.getPageSize());
+        return sysJhAdviceOders;
+    }
+
+    @Override
+    public Integer countListOderAdvice(Long uid) {
+        List<Long> var = new ArrayList();
+        var.add(uid);
+        List<Long> ids = agentDao.queryForAgentIdNew(uid.intValue());
+        if (ids != null) {
+            var.addAll(ids);
+        }
+        Integer sum = adviceDao.countSysOderAdvice(var);
+        return sum;
+    }
 
 }
