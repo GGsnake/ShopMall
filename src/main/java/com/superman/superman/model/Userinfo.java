c7ce5dac9bc1339be3f4b016fb1bc790a237e82f
GGsnake <ljp462693266@outlook.com>
2019-08-21 22:34:35 +0800

    删除无关业务代码

--- src/main/java/com/superman/superman/model/Userinfo.java
+++ src/main/java/com/superman/superman/model/Userinfo.java
@@ -7,7 +7,6 @@ import lombok.ToString;
 
 import java.util.Date;
 
-@Data
 public class Userinfo {
     private Long id;
     private String username;
@@ -61,6 +60,241 @@ public class Userinfo {
     private Integer score;
 
     private String wxopenid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Userinfo setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Userinfo setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getLoginname() {
+        return loginname;
+    }
+
+    public Userinfo setLoginname(String loginname) {
+        this.loginname = loginname;
+        return this;
+    }
+
+    public String getLoginpwd() {
+        return loginpwd;
+    }
+
+    public Userinfo setLoginpwd(String loginpwd) {
+        this.loginpwd = loginpwd;
+        return this;
+    }
+
+    public Long getLoginsecret() {
+        return loginsecret;
+    }
+
+    public Userinfo setLoginsecret(Long loginsecret) {
+        this.loginsecret = loginsecret;
+        return this;
+    }
+
+    public Integer getUsersex() {
+        return usersex;
+    }
+
+    public Userinfo setUsersex(Integer usersex) {
+        this.usersex = usersex;
+        return this;
+    }
+
+    public Integer getUserscore() {
+        return userscore;
+    }
+
+    public Userinfo setUserscore(Integer userscore) {
+        this.userscore = userscore;
+        return this;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public Userinfo setRoleId(Integer roleId) {
+        this.roleId = roleId;
+        return this;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public Userinfo setPid(Integer pid) {
+        this.pid = pid;
+        return this;
+    }
+
+    public String getUserphoto() {
+        return userphoto;
+    }
+
+    public Userinfo setUserphoto(String userphoto) {
+        this.userphoto = userphoto;
+        return this;
+    }
+
+    public String getTree() {
+        return tree;
+    }
+
+    public Userinfo setTree(String tree) {
+        this.tree = tree;
+        return this;
+    }
+
+    public Integer getUserstatus() {
+        return userstatus;
+    }
+
+    public Userinfo setUserstatus(Integer userstatus) {
+        this.userstatus = userstatus;
+        return this;
+    }
+
+    public Long getUsertotalscore() {
+        return usertotalscore;
+    }
+
+    public Userinfo setUsertotalscore(Long usertotalscore) {
+        this.usertotalscore = usertotalscore;
+        return this;
+    }
+
+    public String getJdpid() {
+        return jdpid;
+    }
+
+    public Userinfo setJdpid(String jdpid) {
+        this.jdpid = jdpid;
+        return this;
+    }
+
+    public String getPddpid() {
+        return pddpid;
+    }
+
+    public Userinfo setPddpid(String pddpid) {
+        this.pddpid = pddpid;
+        return this;
+    }
+
+    public Long getTbpid() {
+        return tbpid;
+    }
+
+    public Userinfo setTbpid(Long tbpid) {
+        this.tbpid = tbpid;
+        return this;
+    }
+
+    public String getWphpid() {
+        return wphpid;
+    }
+
+    public Userinfo setWphpid(String wphpid) {
+        this.wphpid = wphpid;
+        return this;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public Userinfo setCreatetime(Date createtime) {
+        this.createtime = createtime;
+        return this;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public Userinfo setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+        return this;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public Userinfo setStatus(Byte status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getUserphone() {
+        return userphone;
+    }
+
+    public Userinfo setUserphone(String userphone) {
+        this.userphone = userphone;
+        return this;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public Userinfo setScore(Integer score) {
+        this.score = score;
+        return this;
+    }
+
+    public String getWxopenid() {
+        return wxopenid;
+    }
+
+    public Userinfo setWxopenid(String wxopenid) {
+        this.wxopenid = wxopenid;
+        return this;
+    }
+
+    public Double getCash() {
+        return cash;
+    }
+
+    public Userinfo setCash(Double cash) {
+        this.cash = cash;
+        return this;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public Userinfo setRid(String rid) {
+        this.rid = rid;
+        return this;
+    }
+
+    public String getSpid() {
+        return spid;
+    }
+
+    public Userinfo setSpid(String spid) {
+        this.spid = spid;
+        return this;
+    }
+
     private Double cash;
     private String rid;
     private String spid;
